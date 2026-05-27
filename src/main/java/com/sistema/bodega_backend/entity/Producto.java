d3feee00a7af56dc31cea62e0f4c8a3cd50ef3cb
umaten <ccjesuslavado@gmail.com>
2026-05-27 12:19:57 -0500

    lombok y demas dependencias

--- src/main/java/com/sistema/bodega_backend/entity/Producto.java
+++ src/main/java/com/sistema/bodega_backend/entity/Producto.java
@@ -5,7 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "productos")//asi se llamara la tabla en mysql
 public class Producto {
@@ -15,39 +23,5 @@ public class Producto {
     private String nombre;
     private double precio;
     private Integer stock;
-
-//Constructores
-    public Producto(){
-    }
-    public Producto(long id,String nombre,double precio,Integer stock){
-        this.id=id;
-        this.nombre = nombre;
-        this.precio=precio;
-        this.stock=stock;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public double getPrecio() {
-        return precio;
-    }
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-    public Integer getStock() {
-        return stock;
-    }
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
     
 }
